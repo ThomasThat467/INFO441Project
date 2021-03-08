689c58eed5b508c38fef54025a88333f295fa914
haileymeister <49212221+haileymeister@users.noreply.github.com>
2021-03-07 18:29:35 -0800

    fix user bugs

---
+++ servers/db/schema.sql
@@ -0,0 +1,23 @@
+create table if not exists Users (
+    ID int not null auto_increment primary key,
+    Email varchar(255) not null unique, /* https://stackoverflow.com/questions/7717573/what-is-the-longest-possible-email-address  */
+    PassHash binary(60) not null, /* https://stackoverflow.com/questions/5881169/what-column-type-length-should-i-use-for-storing-a-bcrypt-hashed-password-in-a-d */
+    UserName varchar(255) not null unique, 
+    FirstName varchar(128) not null,
+    LastName varchar(128) not null,
+    PhotoURL varchar(2083) not null
+);
+
+create table if not exists SignIns (
+  id int not null auto_increment primary key,
+  UserID int not null,
+  SignInTime varchar(255) not null,
+  IP varchar(255) not null
+);
+
+create table if not exists Plants (
+  ID int not null auto_increment primary key,
+  PlantType varchar(255) not null, 
+  WateringSchedule varchar(255) not null,
+  PhotoURL varchar(2083) not null
+);
--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":443"
+	}
+
+	tlsKeyPath := os.Getenv("TLSKEY")
+	tlsCertPath := os.Getenv("TLSCERT")
+
+	sessionKey := os.Getenv("SESSIONKEY")
+	if len(sessionKey) == 0 {
+		log.Fatalf("SESSIONKEY is not set")
+	}
+
+	redisAddr := os.Getenv("REDISADDR")
+	if len(redisAddr) == 0 {
+		log.Fatalf("REDDISADDR is not set")
+	}
+	dsn := os.Getenv("DSN")
+	if len(dsn) == 0 {
+		log.Fatalf("DSN is not set")
+	}
+	// db, err := sql.Open("mysql", dsn)
+	// if err != nil {
+	// 	fmt.Printf("Error opening DB: %v", err)
+	// }
+
+	// redi := redis.NewClient(&redis.Options{
+	// 	Addr: redisAddr,
+	// })
+	//sessionStore := sessions.NewRedisStore(redi, time.Hour)
+	//sql := &users.MySQLStore{Database: db}
+
+	mux := http.NewServeMux()
+
+	log.Printf("Server is listening at %s", addr)
+	log.Fatal(http.ListenAndServeTLS(addr, tlsCertPath, tlsKeyPath, mux))
+}
