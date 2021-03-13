85b06f99c183cd00a095af41a0e9f059b9e6b44a
ThomasThat467 <thomas.tat@hotmail.com>
2021-03-12 19:30:55 -0800

    Got rid of unneeded files from the server and changed README.md

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/ThomasThat467/INFO441Project/tree/main/servers/handlers"
 	"github.com/ThomasThat467/INFO441Project/tree/main/servers/models/plants"
-	"github.com/ThomasThat467/INFO441Project/tree/main/servers/models/schedules"
 	"github.com/ThomasThat467/INFO441Project/tree/main/servers/models/users"
 	"github.com/ThomasThat467/INFO441Project/tree/main/servers/sessions"
 	"github.com/go-redis/redis"
@@ -51,8 +50,7 @@ func main() {
 	sessionStore := sessions.NewRedisStore(redi, time.Hour)
 	userStore := &users.MySQLStore{Database: db}
 	plantStore := &plants.MySQLStore{Database: db}
-	scheduleStore := &schedules.MySQLStore{Database: db}
-	ctx := handlers.NewHandlerContext(sessionKey, sessionStore, userStore, plantStore, scheduleStore)
+	ctx := handlers.NewHandlerContext(sessionKey, sessionStore, userStore, plantStore)
 
 	mux := http.NewServeMux()
 	corsmux := handlers.NewCorsHandler(mux)
@@ -63,8 +61,6 @@ func main() {
 	mux.HandleFunc("/v1/sessions/", ctx.SpecificSessionHandler)
 	mux.HandleFunc("/v1/plant", ctx.PlantHandler)
 	mux.HandleFunc("/v1/plant/", ctx.SpecificPlantHandler)
-	mux.HandleFunc("/v1/schedule", ctx.ScheduleHandler)
-	mux.HandleFunc("/v1/schedule/", ctx.SpecificScheduleHandler)
 
 	log.Printf("Server is listening at %s", addr)
 	log.Fatal(http.ListenAndServeTLS(addr, tlsCertPath, tlsKeyPath, corsmux))
